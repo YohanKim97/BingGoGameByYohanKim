76099eb1c9480004593ec702487e1c91bae642b2
Kim,Donghyun <d_k067@student.usc.edu.au>
2019-02-22 21:41:51 +1000

    Created 5 X 5 BingGo Board... <time.h> and rand have been used to created random numbers between 1 ~ 25. Then, nested for loops to generate bingGo board.

--- BingGoGameByYohanKim/main.cpp
+++ BingGoGameByYohanKim/main.cpp
@@ -1,10 +1,63 @@
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello world " << endl;
 
+	// Creating 5 X 5 BingGo Game
+
+	srand((unsigned int)time(0));
+
+	// 25 numbers
+	int iNumber[25] = {};
+
+	for (int i = 0; i < 25; ++i)
+	{
+		iNumber[i] = i + 1;
+	}
+
+	//Suffling to prevent repeated numbers
+	int iTemp, idx1, idx2;
+	for (int i = 0; i < 100; ++i)
+	{
+		idx1 = rand() % 25;
+		idx2 = rand() % 25;
+
+		iTemp = iNumber[idx1];
+		iNumber[idx1] = iNumber[idx2];
+		iNumber[idx2] = iTemp;
+
+	}
+
+
+
+
+
+	//Print out 5 X 5 BingGo Board
+	cout << "=================== Player ===================" << endl;
+
+	while (true)
+	{
+		for (int i = 0; i < 5; ++i)
+		{
+			for (int j = 0; j < 5; ++j)
+			{
+				
+				if (iNumber[i * 5 + j] == INT_MAX)
+					cout << " * \t ";
+
+				else
+					cout << iNumber[i * 5 + j] << " \t ";
+			}
+
+			cout << endl;
+		
+		}
+		
+		break;
+
+	}
 	return 0;
 }
