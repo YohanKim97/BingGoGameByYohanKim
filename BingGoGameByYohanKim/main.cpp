02a9876e3b97209aae51da773afe9ecc0eee034f
Kim,Donghyun <d_k067@student.usc.edu.au>
2019-02-26 12:12:46 +1000

    Completed AI Easy Mode (which randomly select unselected numbers). Also, Win condition added (if both AI and Player have same lines, player would be the winner)

--- BingGoGameByYohanKim/main.cpp
+++ BingGoGameByYohanKim/main.cpp
@@ -4,6 +4,13 @@
 
 using namespace std;
 
+
+enum GAME_MODE
+{
+	GM_EASY = 1,
+	GM_HARD
+};
+
 int main()
 {
 
@@ -38,6 +45,10 @@ int main()
 		iNumber[idx1] = iNumber[idx2];
 		iNumber[idx2] = iTemp;
 
+		// Shuffling AI Number
+
+		idx1 = rand() % 25;
+		idx2 = rand() % 25;
 
 		iTemp = iAINumber[idx1];
 		iAINumber[idx1] = iAINumber[idx2];
@@ -45,30 +56,55 @@ int main()
 
 	}
 
-
-	////Shuffling AI number
-	//int iTemp, idx1, idx2;
-	//for (int i = 0; i < 100; ++i)
-	//{
-	//	idx1 = rand() % 25;
-	//	idx2 = rand() % 25;
-
-	//	iTemp = iAINumber[idx1];
-	//	iAINumber[idx1] = iAINumber[idx2];
-	//	iAINumber[idx2] = iTemp;
-
-	//}
-
-
 	//Print out 5 X 5 BingGo Board
 	int iBingGoLine = 0;
 	int iAIBingGoLine = 0;
+	int iAIGameMode;
+
+	//Select AI GameMode Level (Either Easy or Hard)
+	while (true)
+	{
+		cout << "1. Easy Game Level" << endl << endl;
+		cout << "2. Hard Game Level" << endl << endl;
+		cout << "Please, select Game Mode" << endl;
+
+		cin >> iAIGameMode; 
+
+
+		if (iAIGameMode >= GM_EASY && iAIGameMode <= GM_HARD)
+		{
+			break;
+		}
+	}
+
+	/*
+		In AI Game Mode, AI will be selecting a number randomly (among the numbers that have not been selected)
+	*/
+
+	// List or Array of unselected numbers
+	int iNonSelectNumber[25] = {};
+
+	// Save list of unselected numbers
+	int iNonSelectNumberCount = 0;
+
 
 	while (true)
 	{
 		system("cls");
 
-		cout << "=================== AI ===================" << endl << endl;
+	
+		cout << "=================== AI ===================" << endl;
+
+		switch (iAIGameMode)
+		{
+		case GM_EASY:
+			cout << " AI GAME MODE : EASY " << endl << endl;
+			break;
+		case GM_HARD:
+			cout << " AI GAME MODE : HARD " << endl << endl;
+			break;
+		}
+
 		cout << " AI BingGo Line : " << iAIBingGoLine << endl << endl;
 		//AI BingGo Board
 		for (int i = 0; i < 5; ++i)
@@ -93,8 +129,8 @@ int main()
 
 
 		cout << "=================== Player ===================" << endl << endl;
-		cout << " Player BingGo Line : " << iBingGoLine << endl << endl;
 
+		cout << " Player BingGo Line : " << iBingGoLine << endl << endl;
 		for (int i = 0; i < 5; ++i)
 		{
 			for (int j = 0; j < 5; ++j)
@@ -112,6 +148,20 @@ int main()
 
 			cout << endl;
 			
+		}
+		
+
+		//Win Condition for Both Player and AI
+		if (iBingGoLine >= 5)
+		{
+			cout << " Player Win!!!!! " << endl;
+			break;
+		}
+
+		else if (iAIBingGoLine >= 5)
+		{
+			cout << " AI Win!!!!!! " << endl;
+			break;
 		}
 
 		cout << "Type Number (0 == EXIT) : ";
@@ -151,6 +201,60 @@ int main()
 			continue;
 		}
 
+		// If Numberis not repeated, chage AI number into " * "
+		for (int i = 0; i < 25; ++i)
+		{
+			if (iAINumber[i] == iInput)
+			{
+				iAINumber[i] = INT_MAX;
+
+				break;
+			}
+		}
+
+		// Function of AI
+		switch (iAIGameMode)
+		{
+		case GM_EASY:
+			// List of unselected number
+			iNonSelectNumberCount = 0;
+			for (int i = 0; i < 25; ++i)
+			{
+				// Current number which is not changed to " * "
+				if (iAINumber[i] != INT_MAX)
+				{
+					iNonSelectNumber[iNonSelectNumberCount] = iAINumber[i];
+					++iNonSelectNumberCount;
+				}
+			}
+
+			iInput = iNonSelectNumber[rand() % iNonSelectNumberCount];
+			break;
+
+		case GM_HARD:
+			break;
+
+		}
+
+		// Change number into " * " selected by AI
+		for (int i = 0; i < 25; ++i)
+		{
+			if (iNumber[i] == iInput)
+			{
+				iNumber[i] = INT_MAX;
+				break;
+			}
+		}
+
+		//
+		for (int i = 0; i < 25; ++i)
+		{
+			if (iAINumber[i] == iInput)
+			{
+				iAINumber[i] = INT_MAX;
+				break;
+			}
+		}
 
 		// BingGO Line Check
 		iBingGoLine = 0;
@@ -161,12 +265,18 @@ int main()
 		for (int i = 0; i < 5; ++i)
 		{
 			iStar1 = iStar2 = 0;
+			iAIStar1 = iAIStar2 = 0;
 			for (int j = 0; j < 5; ++j)
 			{
 				//Row
 				if (iNumber[i * 5 + j] == INT_MAX)
 				{
 					++iStar1;
+				}
+
+				//AI Row
+				if (iAINumber[i * 5 + j] == INT_MAX)
+				{
 					++iAIStar1;
 				}
 
@@ -174,6 +284,11 @@ int main()
 				if (iNumber[j * 5 + i] == INT_MAX)
 				{
 					++iStar2;
+				}
+
+				//AI Column
+				if (iAINumber[j * 5 + i] == INT_MAX)
+				{
 					++iAIStar2;
 				}
 	
@@ -229,6 +344,8 @@ int main()
 		{
 			++iBingGoLine;
 		}
+
+
 
 	}
 	return 0;
