ff82b49775da3b6d26099a0eee94b1e4d67ba750
Kim,Donghyun <d_k067@student.usc.edu.au>
2019-03-04 21:13:35 +1000

    Finished Hard Mode AI. I also add function which take Player name and display it

--- BingGoGameByYohanKim/main.cpp
+++ BingGoGameByYohanKim/main.cpp
@@ -5,10 +5,34 @@
 using namespace std;
 
 
+#define NAME_SIZE 32
+
+struct _tagPlayer
+{
+	char strPlayerName[NAME_SIZE];
+};
+
 enum GAME_MODE
 {
 	GM_EASY = 1,
 	GM_HARD
+};
+
+enum LINE_NUMBER
+{
+	LN_H1,
+	LN_H2,
+	LN_H3,
+	LN_H4,
+	LN_H5,
+	LN_V1, 
+	LN_V2,
+	LN_V3, 
+	LN_V4, 
+	LN_V5,
+	LN_LT,
+	LN_RT
+
 };
 
 int main()
@@ -18,20 +42,23 @@ int main()
 
 	srand((unsigned int)time(0));
 
-	// 25 numbers
+	_tagPlayer tPlayer = {};
+
+	cout << " Player Name :  " << endl;
+
+	cin.getline(tPlayer.strPlayerName, NAME_SIZE - 1);
+
+	// Player and AI BingGO Baord 25 numbers
 	int iNumber[25] = {};
 	int iAINumber[25] = {};
 
 	for (int i = 0; i < 25; ++i)
 	{
 		iNumber[i] = i + 1;
-	}
-
-	//AI 25 numbers
-	for (int i = 0; i < 25; ++i)
-	{
 		iAINumber[i] = i + 1;
 	}
+
+	
 
 
 	//Shuffling to prevent repeated numbers
@@ -64,6 +91,8 @@ int main()
 	//Select AI GameMode Level (Either Easy or Hard)
 	while (true)
 	{
+		system("cls");
+
 		cout << "1. Easy Game Level" << endl << endl;
 		cout << "2. Hard Game Level" << endl << endl;
 		cout << "Please, select Game Mode" << endl;
@@ -92,9 +121,32 @@ int main()
 	{
 		system("cls");
 
-	
-		cout << "=================== AI ===================" << endl;
+		cout << "=================== Player ===================" << endl << endl;
 
+		cout << " Name :  " << tPlayer.strPlayerName << endl << endl;
+		cout << " Player BingGo Line : " << iBingGoLine << endl << endl;
+		
+		for (int i = 0; i < 5; ++i)
+		{
+			for (int j = 0; j < 5; ++j)
+			{
+
+				if (iNumber[i * 5 + j] == INT_MAX)
+				{
+					cout << " * \t ";
+				}
+				else
+				{
+					cout << iNumber[i * 5 + j] << " \t ";
+				}
+			}
+
+			cout << endl;
+
+		}
+
+		cout << "=================== AI ===================" << endl;
+		cout << " AI BingGo Line : " << iAIBingGoLine << endl << endl;
 		switch (iAIGameMode)
 		{
 		case GM_EASY:
@@ -105,7 +157,7 @@ int main()
 			break;
 		}
 
-		cout << " AI BingGo Line : " << iAIBingGoLine << endl << endl;
+		
 		//AI BingGo Board
 		for (int i = 0; i < 5; ++i)
 		{
@@ -126,30 +178,6 @@ int main()
 
 		}
 	
-
-
-		cout << "=================== Player ===================" << endl << endl;
-
-		cout << " Player BingGo Line : " << iBingGoLine << endl << endl;
-		for (int i = 0; i < 5; ++i)
-		{
-			for (int j = 0; j < 5; ++j)
-			{
-				
-				if (iNumber[i * 5 + j] == INT_MAX)
-				{
-					cout << " * \t ";
-				}
-				else
-				{
-					cout << iNumber[i * 5 + j] << " \t ";
-				}
-			}
-
-			cout << endl;
-			
-		}
-		
 
 		//Win Condition for Both Player and AI
 		if (iBingGoLine >= 5)
@@ -208,6 +236,8 @@ int main()
 			{
 				iAINumber[i] = INT_MAX;
 
+				--iNonSelectNumberCount;
+
 				break;
 			}
 		}
@@ -229,9 +259,137 @@ int main()
 			}
 
 			iInput = iNonSelectNumber[rand() % iNonSelectNumberCount];
+
 			break;
 
 		case GM_HARD:
+
+			// In Hard Mode, AI will select the number
+			// Which has the highest percentage of get a BingGo Line	
+
+			int iLine = 0;
+			int iStarCount = 0;
+
+			// Save the line which has the most number of " * "
+			int iSaveCount = 0;
+
+			//Search the most of number of Star line among Row and Column line
+			for (int i = 0; i < 5; ++i)
+			{
+				iStarCount = 0;
+				for (int j = 0; j < 5; ++j)
+				{
+					if (iAINumber[i * 5 + j] == INT_MAX)
+						++iStarCount;
+				}
+
+
+				if (iStarCount < 5 && iSaveCount < iSaveCount)
+				{
+					iLine = i;
+					iSaveCount = iSaveCount;
+				}
+
+			}
+
+			for (int i = 0; i < 5; ++i)
+			{
+				for (int j = 0; j < 5; ++j)
+				{
+					if (iAINumber[j * 5 + i] == INT_MAX)
+					{
+						++iSaveCount;
+					}
+				}
+				
+				if (iStarCount < 5 && iSaveCount < iStarCount)
+				{
+					iLine = i + 5;
+					iSaveCount = iStarCount;
+				}
+
+			} 
+
+			iStarCount = 0;
+			
+			for (int i = 0; i < 25; i += 6)
+			{
+				if (iAINumber[i] == INT_MAX)
+				{
+					++iStarCount;
+				}
+			}
+
+			if (iStarCount < 5 && iSaveCount < iStarCount)
+			{
+				iLine = LN_LT;
+				iSaveCount = iStarCount;
+			}
+
+			iStarCount = 0;
+
+			for (int i = 0; i <= 20; i += 4)
+			{
+				if (iAINumber[i] == INT_MAX)
+				{
+					++iStarCount;
+				}
+			}
+
+			if (iStarCount < 5 && iSaveCount < iStarCount)
+			{
+				iLine = LN_RT;
+				iSaveCount = iStarCount;
+			}
+
+			if (iLine <= LN_H5)
+			{
+				for (int i = 0; i < 5; ++i)
+				{
+					// Find number that is not a " * " in a line
+					if (iAINumber[iLine * 5 + i] != INT_MAX)
+					{
+						iInput = iAINumber[iLine * 5 + i];
+						break;
+					}
+				}
+			}
+
+			else if (iLine <= LN_V5)
+			{
+				for (int i = 0; i < 5; ++i)
+				{
+					if (iAINumber[i * 5 + (iLine - 5)] != INT_MAX)
+					{
+						iInput = iAINumber[i * 5 + (iLine - 5)];
+						break;
+					}
+				}
+			}
+
+			else if (iLine == LN_LT)
+			{
+				for (int i = 0; i < 25; i += 6)
+				{
+					if (iAINumber[i] != INT_MAX)
+					{
+						iInput = iAINumber[i];
+						break;
+					}
+				}
+			}
+			
+			else if (iLine == LN_RT)
+			{
+				for (int i = 0; i <= 20; i += 4)
+				{
+					if (iAINumber[i] != INT_MAX)
+					{
+						iInput = iAINumber[i];
+						break;
+					}
+				}
+			}
 			break;
 
 		}
@@ -262,31 +420,32 @@ int main()
 
 		int iStar1 = 0, iStar2 = 0;
 		int iAIStar1 = 0, iAIStar2 = 0;
+
 		for (int i = 0; i < 5; ++i)
 		{
 			iStar1 = iStar2 = 0;
 			iAIStar1 = iAIStar2 = 0;
 			for (int j = 0; j < 5; ++j)
 			{
-				//Row
+				// Player Horizontal Line
 				if (iNumber[i * 5 + j] == INT_MAX)
 				{
 					++iStar1;
 				}
 
-				//AI Row
-				if (iAINumber[i * 5 + j] == INT_MAX)
-				{
-					++iAIStar1;
-				}
-
-				//Column
+				// Player Vertical Line
 				if (iNumber[j * 5 + i] == INT_MAX)
 				{
 					++iStar2;
 				}
 
-				//AI Column
+				// AI Horizontal Line
+				if (iAINumber[i * 5 + j] == INT_MAX)
+				{
+					++iAIStar1;
+				}
+
+				//AI Vertical Line
 				if (iAINumber[j * 5 + i] == INT_MAX)
 				{
 					++iAIStar2;
@@ -319,11 +478,14 @@ int main()
 
 		// Uper Left -> Lower Right
 		iStar1 = 0;
-		iStar2 = 0;
+		iAIStar1 = 0;
 		for (int i = 0; i < 25; i += 6)
 		{
 			if (iNumber[i] == INT_MAX)
 				++iStar1;
+
+			if (iAINumber[i] == INT_MAX)
+				++iAIStar1;
 		}
 
 		if (iStar1 == 5)
@@ -331,13 +493,21 @@ int main()
 			++iBingGoLine;
 		}
 
+		if (iAIStar1 == 5)
+		{
+			++iAIBingGoLine;
+		}
+
 		// Uper Right -> Lower Left
 		iStar1 = 0;
-		iStar2 = 0;
+		iAIStar1 = 0;
 		for (int i = 0; i <= 20; i += 4)
 		{
 			if (iNumber[i] == INT_MAX)
-				++iStar2;
+				++iStar1;
+
+			if (iAINumber[i] == INT_MAX)
+				++iAIStar1;
 		}
 
 		if (iStar2 == 5)
@@ -345,6 +515,10 @@ int main()
 			++iBingGoLine;
 		}
 
+		if (iAIStar2 == 5)
+		{
+			++iAIBingGoLine;
+		}
 
 
 	}
